d579cfab2d2242a8b1b241010c4dd0695e5cc4c5
rngnrs <sup@tumba.ch>
2021-07-04 09:17:10 +0700

    Update PostModelInterface due to PostModelPostgre changes

--- app/Domain/PostModelInterface.js
+++ app/Domain/PostModelInterface.js
@@ -5,6 +5,7 @@ class PostModelInterface {
   async readOneByThreadId(threadId) {}
   async readOneByBoardAndPost(boardName, number) {}
   async readByThreadId(threadId, { count, page, order } = {}) {}
+  async readByBoardNameAndThreadNumber(boardName, { count, page, order } = {}) {}
   async readByBoardName(boardName, { count, page, order } = {}) {}
   async countByThreadId(threadId) {}
   async update(post) {}
