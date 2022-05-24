fbe90846589b44a6ca14ededbfce16a8d7082b25
rngnrs <sup@tumba.ch>
2022-05-24 23:01:59 +0700

    Rewrite postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,32 +1,31 @@
 const fs = require('fs').promises;
 
-const src = 'app/Infrastructure/Config.js'
-const dst = 'config.js'
+const src = 'app/Infrastructure/Config.js';
+const dst = 'config.js';
 
-fs.readFile(dst, 'utf8')
-  // Check if config already exists
-  .then(() => {
-    console.debug('Config file already exists. No action needed.')
-    process.exit(0);
-  })
-  .catch(() => null)
+(async () => {
+  try {
+    let file = await fs.readFile(dst, 'utf8');
+    // Check if config already exists
+    if (file) {
+      console.debug('Config file already exists. No action needed.');
+      process.exit(0);
+    }
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      console.error(e);
+    }
+  }
   // Read template
-  .then(() => (
-    fs.readFile(src, 'utf8')
-  ))
+  let configTemplate = await fs.readFile(src, 'utf8');
   // Fix template
-  .then((configTemplate) => (
-    configTemplate
+  let config = configTemplate
       .replace(/\.\.\/\.\./gm, '.')
       .split('\n')
       .filter(line => !line.includes('Figurecon'))
       .join('\n')
-      + 'module.exports = config;\n'
-  ))
+    + 'module.exports = config;\n';
   // Write default config
-  .then((config) => (
-    fs.writeFile(dst, config)
-  ))
-  .then(() => (
-    console.debug('Created a new config from template.')
-  ))
+  await fs.writeFile(dst, config);
+  console.debug('Created a new config from template.')
+})();
