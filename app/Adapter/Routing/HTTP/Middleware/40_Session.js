0d7453fd819e2adbdd6415e8a92daf71d7deb405
rngnrs <sup@tumba.ch>
2022-12-06 22:29:20 +0700

    Remove setting a cookie from OPTIONS requests

--- app/Adapter/Routing/HTTP/Middleware/40_Session.js
+++ app/Adapter/Routing/HTTP/Middleware/40_Session.js
@@ -6,7 +6,7 @@ let middleware = app => {
   app.keys = config.get('cookie.keys');
   app.use(Session);
   app.use(async (ctx, next) => {
-    if (!ctx.session?.key) {
+    if (['GET', 'POST'].includes(ctx.request.method.toLocaleUpperCase()) && !ctx.session?.key) {
       ctx.session.key = Tools.randomSessionString();
     }
     await next();
